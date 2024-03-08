2774a0d529c74bc8cb524a3cb05d137f29b37556
dTMP3st <ThiagoMirandaDePaula@gmail.com>
2024-03-08 16:37:19 -0300

    Updated code.

--- RL_Scan.sh
+++ RL_Scan.sh
@@ -5,6 +5,8 @@ echo ""
 
 Rate_Limit_Implemented () {
         Time_Return=$(date | cut -d " " -f5)
+        echo "Requisições com sucesso: $Request_Success"
+        echo "Requisições com falha: $Request_Failure"
         setterm -foreground red && echo "	[+] The limit of requests to the API before the Rate Limit control starts acting is $Request_Success Requests."
         setterm -foreground blue && echo "	[*] Start Time: $Time"
         setterm -foreground red && echo "	[*] Stop time: $Time_Out"
@@ -28,6 +30,8 @@ Rate_Limit_Implemented () {
 
 Rate_Limit_Failure (){
         Time_Return=$(date | cut -d " " -f5)
+        echo "Requisições com sucesso: $Request_Success"
+        echo "Requisições com falha: $Request_Failure"
         setterm -foreground blue && echo "	[*] Start Time: $Time"
 	setterm -foreground blue && echo "	[+] Test Mode: Non-Authenticated"
         setterm -foreground red && echo "	[+] There is no rate limit control implemented for API protection"
@@ -45,6 +49,7 @@ echo ""
 echo "Developed by > dTMP3st"
 echo "Cyber Strike Force"
 echo ""
+echo "[+]-------------------------------------------------[+]" # >> /dev/null
 Request_Success="0"
 Request_Failure="0"
 Time=$(date | cut -d " " -f5)
@@ -55,8 +60,6 @@ Request=$(curl -sIkX GET -A "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/201
                 echo "		Request Number [$Loop] - [SUCESS]" # >> /dev/null
 		echo "[+]-------------------------------------------------[+]" # >> /dev/null
                         if [ "$Loop" == "1000" ] && [ "$Loop" == "$Request_Success" ]; then
-                                echo "Requisições com sucesso: $Request_Success"
-                                echo "Requisições com falha: $Request_Failure"
                                 Rate_Limit_Failure
                         else
                                 echo "Wait" >> /dev/null
@@ -101,8 +104,18 @@ Request=$(curl -sIkX GET -A "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/201
                         fi
                 done
         else
-                echo "An error has occurred"
-                echo "Status code different 200 or 429"
+                echo "An error has occurred" >> /dev/null
+                Request_Failure=$(echo "$Request_Failure"+1|bc)
+                echo "Request Number [$Loop] - [FAILURE]" # >> /dev/null
+                if [ "$Loop" == "1000" ]; then
+                        Request_Failure=$(echo "$Request_Failure"+1|bc)
+                        echo "Request Number [$Loop] - [FAILURE]" # >> /dev/null
+		        echo "[+]-------------------------------------------------[+]" # >> /dev/null
+                        echo ""
+                        Rate_Limit_Implemented
+                else
+                        echo "Wait" >> /dev/null
+                fi
         fi
 done
 
